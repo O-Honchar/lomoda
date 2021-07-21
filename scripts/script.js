19eca8e1617ed5106ba867808830882b44eeedfe
Oleh Honchar <O.O.Honchar@gmail.com>
2021-07-21 21:26:59 +0300

    feat: implement the ability to open and close cart modal window

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const headerCityButton = document.querySelector('.header__city-button');
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
 
 // ------- city button -------
 headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
@@ -17,4 +19,33 @@ const headerCityButtonHandler = () => {
     }
 };
 
+// ------- cart modal window -------
+const cartModalOpenHandler = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+};
+
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+};
+
+const cartModalCloseHandler = event => {
+    const target = event.target;
+    if (target.matches('.cart__btn-close') || target.matches('.cart-overlay')) {
+        cartModalClose();
+    }
+};
+
+const cartModalCloseByEscHandler = event =>  {
+    const eventKey = event.key || event.code;
+    if (eventKey === 'Escape') {
+        cartModalClose();
+    }
+};
+
 headerCityButton.addEventListener('click', headerCityButtonHandler);
+
+subheaderCart.addEventListener('click', cartModalOpenHandler);
+
+cartOverlay.addEventListener('click', cartModalCloseHandler);
+
+document.addEventListener('keydown', cartModalCloseByEscHandler);
