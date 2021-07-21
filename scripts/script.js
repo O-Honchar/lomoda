210f9370715035f5f2ac1de3e465102d7af281dc
Oleh Honchar <O.O.Honchar@gmail.com>
2021-07-21 21:00:14 +0300

    feat: implement the ability to type city name and save it in the Local Storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const headerCityButton = document.querySelector('.header__city-button');
+
+// ------- city button -------
+headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
+
+const capitalize = (str) =>
+    str.replace(/(^|\s)\S/g, letter => letter.toUpperCase());
+
+const headerCityButtonHandler = () => {
+    let city = prompt('Укажите Ваш город:')?.trim(); 
+    if (city) {
+        city = capitalize(city);
+        headerCityButton.textContent = city;
+        localStorage.setItem('lomoda-location', city);
+    }
+};
+
+headerCityButton.addEventListener('click', headerCityButtonHandler);
